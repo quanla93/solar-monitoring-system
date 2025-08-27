597bd9c1860b87d4ebdd507d277a4d563372ed02
QuanLA <quanla.work@gmail.com>
2025-08-27 18:11:33 +0700

    SMS_Initialize_project

---
+++ src/test/java/com/solar_monitoring_system/solar_monitoring_system/SolarPowerPlantMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar_monitoring_system.solar_monitoring_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarPowerPlantMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
